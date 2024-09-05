0aa26bfa80e69b76e082363f786dc364fe2a1a74
Augusto Koch <augustok872@gmail.com>
2024-09-05 16:20:34 -0300

    fix(controllers): Removidas as funções que interagiam com atributos excluidos

--- src/main/java/com/biblioteca/demo/controller/UsuarioController.java
+++ src/main/java/com/biblioteca/demo/controller/UsuarioController.java
@@ -40,8 +40,6 @@ public class UsuarioController {
             Usuario updatedUsuario = usuario.get();
             updatedUsuario.setNome(usuarioDetails.getNome());
             updatedUsuario.setEmail(usuarioDetails.getEmail());
-            updatedUsuario.setSenha(usuarioDetails.getSenha());
-            updatedUsuario.setRole(usuarioDetails.getRole());
             return ResponseEntity.ok(usuarioService.save(updatedUsuario));
         } else {
             return ResponseEntity.notFound().build();
