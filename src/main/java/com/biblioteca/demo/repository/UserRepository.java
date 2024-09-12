4523ee2b9a27666a5a8e922ee7b5015a8d93a214
brunoamarilha <brunnoamarilha2010@gmail.com>
2024-09-12 19:56:47 -0300

    feat(respository) foi criado um userReepository

---
+++ src/main/java/com/biblioteca/demo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.biblioteca.demo.repository;
+
+
+
+import com.biblioteca.demo.entity.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UserRepository extends JpaRepository<User, String> {
+    UserDetails findByLogin(String login);
+}
